af3e9b34a530c54a216db398eb0e9ffaae252734
Maryana Dey <Dey.maryana1989@gmail.com>
2023-08-01 21:32:57 +0200

    add HERO

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,62 @@ window.addEventListener("load", function () {
 
 
 
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
 
 
 
+/**
+ * NAVBAR
+ */
 
 
 
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.toggle("nav-active");
+}
+
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+/**
+ * HEADER
+ */
+
+const header = document.querySelector("[header]");
+
+let lastScrollPost = 0;
+
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPost < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+  lastScrollPost = window.scrollY;
+}
+
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+  }
+})
